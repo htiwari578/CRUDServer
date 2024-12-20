f4781a6dd08e2da644461bd2453e6e045afe6da1
Himanshu Tiwari <htiwari578@gmail.com>
2024-12-20 22:43:12 +0530

    Postgres connection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from "express" ;
 
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/db.js";
 
 
 dotenv.config();
@@ -12,7 +13,7 @@ const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 const PORT  = process.env.PORT || 3000 ;
 
@@ -21,8 +22,17 @@ const PORT  = process.env.PORT || 3000 ;
 
 // error handling
 
+// Testing POSTGRES connection
+app.get("/", async(req, res)=> {
+    console.log("Start");
+    const result = await pool.query("SELECT current_database()");
+    console.log("end");
+    res.send(`The database name is: ${result.rows[0].current_database}`);
+})
+
 
 app.listen(PORT, ()=> {
+ 
     console.log(`server is runing at port  ${PORT}`);
 })
 
